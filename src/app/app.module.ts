552a1ab0de6365c981cdfd9f646ae35eeecd6e10
cfsfines <cfsfines99@gmail.com>
2024-09-16 10:39:35 -0700

    creation of skeleton ui for home and leaderboards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,24 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { ButtonComponent } from "./components/button/button.component";
+import { LeaderboardComponent } from "./leaderboard/leaderboard.component";
+import { PlayComponent } from './play/play.component';
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "play", component: PlayComponent },
+  { path: "leaderboard", component: LeaderboardComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ButtonComponent,
+    LeaderboardComponent,
+    PlayComponent,
+  ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
