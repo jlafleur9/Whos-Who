fa29f6a19a8b8c0f876ed689ac274ef855705e15
cfsfines <cfsfines99@gmail.com>
2024-09-18 14:09:05 -0700

    visual changes throughout, error handling for play page

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,13 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ButtonComponent implements OnInit {
   
   @Input() buttonText: string | undefined
+  @Input() disabled: boolean | undefined;
   @Output() click = new EventEmitter<void>();
 
   handleClick(): void {
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -99,9 +99,10 @@ export class PlayComponent implements OnInit {
     }
   }
 
-  setAnswer(selectedArtist: string): void {
-    this.selectedAnswer = selectedArtist;
-    // console.log(this.track.album.images[1])
+  setAnswer(option: string): void {
+    if (!this.selectedAnswer) {
+      this.selectedAnswer = option;
+    }
   }
 
   handleContinue(): void {
