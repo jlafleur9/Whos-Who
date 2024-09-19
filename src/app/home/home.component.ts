60c90ffc8e56c7ca0f90d0d1cccd67404b57882b
Jeffrey LaFleur <jeffreylafleur9@gmail.com>
2024-09-19 01:41:35 -0700

    changes to how the api is used

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,35 +21,6 @@ export class HomeComponent implements OnInit {
   authLoading: boolean = false;
   configLoading: boolean = false;
   token: String = "";
-  artistList: { [key: string]: string[] } = {
-    Pop: ["Taylor Swift", "Justin Bieber"],
-    Rock: [
-      "Linkin Park",
-      "The Neighbourhood",
-      "Deftones",
-      "Green Day",
-      "Red Hot Chili Peppers",
-      "Metallica",
-      "Radiohead",
-      "Dominic Fike",
-      "System Of A Down",
-      "Nickelback",
-      "Paramore",
-      "Slipknot",
-      "Gorillaz",
-      "Cage The Elephant",
-      "My Chemical Romance",
-      "Three Days Grace",
-      "Bring Me The Horizon",
-      "Led Zeppelin",
-      "AC/DC",
-      "Foo Fighters",
-      "Muse",
-      "Alice in Chains",
-      "Rage Against the Machine",
-    ],
-    ['J-pop']: ["YOASOBI", "RADWIMPS", "Creepy Nuts", "Aimer", "Kenshi Yonezu"],
-  };
 
   constructor(private router: Router, private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -113,134 +84,36 @@ export class HomeComponent implements OnInit {
     console.log(this.selectedDifficulty);
   }
   startGame() {
-    // if (!this.selectedGenre || !this.selectedDifficulty) {
-    //   return;
-    // }
     if (this.form.valid) {
-      this.fetchTracks();
+      this.playGame();
     } else {
-      this.showError = true; // Show error message if form is invalid
+      this.showError = true;
     }
   }
-
-  fetchTracks(attempts = 1) {
-    const maxAttempts = 5;
-    const artists = this.artistList[this.selectedGenre as string];
-    const randomArtist = artists[Math.floor(Math.random() * artists.length)];
-    const endpoint = `search`;
-    const params = {
-      //q: 'Creedence Clearwater Revival',
-      q: `artist:${randomArtist}`,
-      type: "track",
-      limit: 1,
-    };
-    fetchFromSpotify({ token: this.token, endpoint, params })
-      .then((data: any) => {
-        const track = data.tracks.items[0];
-        if (track && track.preview_url) {
-          this.navigateToPlayPage(track, artists);
-        } else {
-          console.log(data);
-          console.error("No track preview available for the selected artist.");
-          if (attempts < maxAttempts) {
-            console.log("retrying");
-            this.fetchTracks(attempts + 1);
-          }
-        }
-      })
-      .catch((error: any) => {
-        console.error("Error fetching tracks:", error);
-        if (attempts < maxAttempts) {
-          console.log("retrying");
-          this.fetchTracks(attempts + 1);
-        }
-      });
-  }
-
-  navigateToPlayPage(track: any, artists: string[]) {
-    const numOptions = this.getNumberOfChoices();
-    const correctArtist = track.artists[0].name;
-
-    const wrongChoices = artists.filter((artist) => artist !== correctArtist);
-    const shuffledChoices = this.shuffleArray([...wrongChoices]).slice(
-      0,
-      numOptions - 1
-    );
-    shuffledChoices.push(correctArtist);
-
-    const options = this.shuffleArray(shuffledChoices);
-
+  playGame(){
     console.log("Navigating to play with state:", {
-      track: track,
-      options: options,
-      correctArtist: correctArtist,
+      difficulty: this.selectedDifficulty,
       genre: this.selectedGenre,
       token: this.token,
     });
-
-    this.router.navigate(["play"], {
+    this.router.navigate(['play'],{
       state: {
-        track: track,
-        options: options,
-        diffculty: this.selectedDifficulty,
-        correctArtist: correctArtist,
+        choices: this.getNumberOfChoices(),
         genre: this.selectedGenre,
-        token: this.token,
-      },
-    });
+        token: this.token
+      }
+    })
   }
-
   getNumberOfChoices() {
     switch (this.selectedDifficulty) {
-      case "easy":
+      case 'easy':
         return 3;
-      case "medium":
+      case 'medium':
         return 4;
-      case "hard":
+      case 'hard':
         return 5;
       default:
         return 4;
     }
   }
-
-  shuffleArray(array: any[]) {
-    return array.sort(() => Math.random() - 0.5);
-  }
 }
-
-//Im going to leave the code they provided in comments bellow if in case we need it
-
-// loadGenres = async (t: any) => {
-//   this.configLoading = true;
-
-//   // #################################################################################
-//   // DEPRECATED!!! Use only for example purposes
-//   // DO NOT USE the recommendations endpoint in your application
-//   // Has been known to cause 429 errors
-//   // const response = await fetchFromSpotify({
-//   //   token: t,
-//   //   endpoint: "recommendations/available-genre-seeds",
-//   // });
-//   // console.log(response);
-//   // #################################################################################
-
-//   this.genres = [
-//     "rock",
-//     "rap",
-//     "pop",
-//     "country",
-//     "hip-hop",
-//     "jazz",
-//     "alternative",
-//     "j-pop",
-//     "k-pop",
-//     "emo"
-//   ]
-//   this.configLoading = false;
-// };
-
-// setGenre(selectedGenre: any) {
-//   this.selectedGenre = selectedGenre;
-//   console.log(this.selectedGenre);
-//   console.log(TOKEN_KEY);
-// }
